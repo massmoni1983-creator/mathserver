1fe08690c854e3a4f1c32a815cc905f20423db55
massmoni1983-creator <massmoni1983@gmail.com>
2025-10-08 14:49:05 +0530

    success

---
+++ muruga/mathapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def index(request):
+    result = None
+    if request.method == "POST":
+        try:
+            I = float(request.POST.get("intensity"))
+            R = float(request.POST.get("resistance"))
+            result = (I ** 2) * R
+        except:
+            result = "Invalid input"
+    return render(request, "mathapp/math.html", {"result": result})
+
+
